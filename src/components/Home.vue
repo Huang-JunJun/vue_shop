506471b9f1f4d7d931e008511c09499f27132a58
黄显竣 <770894595@qq.com>
2021-06-02 14:42:36 +0800

    完成退出登录

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,18 @@
 <template>
     <div>
         home组件
+        <el-button type="info" @click="exitLogin()">退出</el-button>
     </div>
 </template>
 
 <script>
 export default {
-    
+    methods: {
+        exitLogin() {
+            window.sessionStorage.clear()
+            this.$router.push("/login")
+        }
+    }
 }
 </script>
 
